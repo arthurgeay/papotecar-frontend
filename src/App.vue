e18983e54e5e8e59c565110ac2e0e439dde14f0a
arthurgeay <arthurgeay.contact@gmail.com>
2023-02-09 00:49:47 +0100

    chore: add tailwind + flowbite + tailwind prettier

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Papote car</h1>
+  <h1
+    class="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl"
+  >
+    PapoteCar
+  </h1>
 </template>
 
 <script setup></script>
