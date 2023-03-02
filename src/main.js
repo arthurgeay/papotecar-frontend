c79d94138cc6a7ec824e31709f8e81aa1cd9d17d
Auriane Labille <auriane.labille@gmail.com>
2023-03-02 11:52:49 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Login from './views/Login.vue'
+import Register from './views/Register.vue'
 
 import './style.css'
 import 'flowbite'
