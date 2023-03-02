70f6cd6eea50278f88dacb2c6cd6a450c122d552
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2023-03-02 12:00:07 +0100

    fix: styling on homepage

--- src/views/SearchTrip.vue
+++ src/views/SearchTrip.vue
@@ -1,13 +1,12 @@
 <template>
     <NavBar/>
     <div class="hero">
-        <div class="hero__container">
+        <div class="hero__container px-2 sm:px-4 py-2.5 fixed w-full">
             <h1 class="hero__container__title text-4xl font-bold text-white">Conduisez, voyagez,<br/> papotez !</h1>
         </div>
     </div>
     <div class="search">
-        <div
-            class="search__container block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div class="search__container max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div class="search__container__input">
                 <label for="starting-location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
                 <input id="starting-location" type="text" placeholder="Starting location..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
@@ -67,5 +66,8 @@ export default {
     width: 100%;
     height: 100%;
     padding: 0 1rem;
+}
+.search__container__input {
+    margin: 0 .5rem;
 }
 </style>
