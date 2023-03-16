e482d8e9a50275f2b2be810d084a6629ad0c07f6
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2023-03-16 15:57:23 +0100

    💬 displaying result

--- src/components/ResultElement.vue
+++ src/components/ResultElement.vue
@@ -1,69 +1,64 @@
 <template>
     <div class="resultElement">
         <div class="resultElement__frame max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <ol class="resultElement__container relative border-l border-gray-200 dark:border-gray-700">
-            <li class="mb-10 ml-6">
-                <span
-                    class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900">
-                    <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </span>
-                <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{{ result.departure_datetime }}</h3>
-                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ result.departure_location.name }}</p>
-            </li>
-            <li class="ml-6">
-                <span
-                    class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900">
-                    <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor"
-                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </span>
-                <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">23H45
-                </h3>
-                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ result.arrival_location.name }}</p>
-            </li>
-        </ol>
-        <div class="resultElement_trip">
-            <div class="resultElement_trip__passagers">
-                <p class="font-bold">Nombre de passagers maximum</p>
-                <p class="number">{{ result.max_passengers }}</p>
-            </div>
-            <div class="resultElement_trip__price">
-                <p class="font-bold">Prix</p>
-                <p class="price">{{result.price}}€</p>
-            </div>
-            <div class="resultElement_trip__description">
-                <p class="font-bold">Description</p>
-                <p class="description">{{ result.content }}</p>
+            <ol class="resultElement__container relative border-l border-gray-200 dark:border-gray-700">
+                <li class="mb-10 ml-6">
+                    <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900">
+                        <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
+                        </svg>
+                    </span>
+                    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{{ result.departure_datetime }}</h3>
+                    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ result.departure_location.name }}</p>
+                </li>
+                <li class="ml-6">
+                    <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900">
+                        <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
+                        </svg>
+                    </span>
+                    <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{{ arrival_datetime }}
+                    </h3>
+                    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{{ result.arrival_location.name }}</p>
+                </li>
+            </ol>
+            <div class="resultElement_trip">
+                <div class="resultElement_trip__passagers">
+                    <p class="font-bold">Nombre de passagers maximum</p>
+                    <p class="number">{{ result.max_passengers }}</p>
+                </div>
+                <div class="resultElement_trip__price">
+                    <p class="font-bold">Prix</p>
+                    <p class="price">{{ result.price }}€</p>
+                </div>
+                <div class="resultElement_trip__description">
+                    <p class="font-bold">Description</p>
+                    <p class="description">{{ result.content }}</p>
+                </div>
+
             </div>
 
-        </div>
-        
-        <div class="resultElement_information">
-            <div class="resultElement_information__container">
-                <div class="resultElement_information__container__profilePicture">
-                    <img :src="'https://i.pravatar.cc/150?u='+ result.driver.id" alt="profile picture">
+            <div class="resultElement_information">
+                <div class="resultElement_information__container">
+                    <div class="resultElement_information__container__profilePicture">
+                        <img :src="'https://i.pravatar.cc/150?u=' + result.driver.id" alt="profile picture">
+                    </div>
+                    <div class="resultElement_information__container__name">
+                        <p>{{ result.driver.fullname }}</p>
+                    </div>
                 </div>
-                <div class="resultElement_information__container__name">
-                    <p>{{result.driver.fullname}}</p>
-                </div>
+                <button type="button" class="resultElement_information__button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Réserver</button>
             </div>
-            <button type="button" class="resultElement_information__button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Réserver</button>
         </div>
-    </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "ResultElement",
+    data: () => ({
+        res: null
+    }),
     props: {
         result: {
             type: Object,
@@ -71,7 +66,22 @@ export default {
         }
     },
     mounted() {
+        const res = JSON.parse(JSON.stringify(this.result))
         this.result.departure_datetime = this.result.departure_datetime.split("T")[1].split(":")[0] + "H" + this.result.departure_datetime.split("T")[1].split(":")[1];
+        this.getData();
+    },
+    methods: {
+         getData : async () => {
+            const coordinates = `${result.departure_location.coordinates.longitude},${result.departure_location.coordinates.latitude};${result.arrival_location.coordinates.longitude},${result.arrival_location.coordinates.latitude}`;
+            const result = await fetch(
+                `https://api.mapbox.com/directions/v5/mapbox/driving/${coordinates}?alternatives=false&geometries=geojson&language=fr-FR&overview=simplified&steps=true&access_token=${import.meta.env.VITE_API_MAPBOX_ACCESS_TOKEN
+                }`
+            );
+
+            const data = await result.json();
+            console.log({ result: data });
+            console.log(data.routes[0].distance.toLocaleString("fr-FR"));
+        }
     }
 };
 </script>
@@ -84,23 +94,28 @@ export default {
     width: 100%;
     padding: 1rem;
 }
+
 .resultElement__frame {
     display: flex;
     flex-direction: row;
 }
+
 .resultElement__container,
 .resultElement_trip {
     margin: 0 1rem;
 }
-.resultElement_trip > div:not(:last-child) {
+
+.resultElement_trip>div:not(:last-child) {
     margin-bottom: 1rem;
 }
+
 .resultElement_information {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
 }
+
 .resultElement_information__container {
     display: flex;
     flex-direction: column;
@@ -110,32 +125,38 @@ export default {
     padding: 1rem;
     border-radius: .5rem;
 }
+
 .resultElement_information__button {
     margin-top: 1rem;
 }
+
 .resultElement_information__container__profilePicture {
     display: flex;
     justify-content: center;
     min-width: 5rem;
 }
+
 .resultElement_information__container__name {
     display: flex;
     align-items: center;
     justify-content: center;
     margin-top: 1rem;
 }
+
 .resultElement_information__container img {
     border-radius: 50%;
 }
+
 @media (max-width: 1200px) {
     .resultElement__frame {
         flex-direction: column;
     }
+
     .resultElement_trip {
         margin: 0;
     }
+
     .resultElement_information {
         margin-top: 1rem;
     }
-}
-</style>
+}</style>
--- src/views/SearchTrip.vue
+++ src/views/SearchTrip.vue
@@ -1,35 +1,37 @@
 <template>
-    <NavBar/>
+    <NavBar />
     <div class="hero">
         <div class="hero__container px-2 sm:px-4 py-2.5 w-full">
-            <h1 class="hero__container__title text-6xl font-bold text-white">Conduisez, voyagez,<br/> papotez !</h1>
+            <h1 class="hero__container__title text-6xl font-bold text-white">Conduisez, voyagez,<br /> papotez !</h1>
         </div>
     </div>
     <div class="search">
-        <div class="search__container max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <div class="search__container__input">
-                <label for="starting-location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                <input v-model="start" id="starting-location" type="text" placeholder="Lieu de départ..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        <form @submit.prevent="getTrips()">
+            <div class="search__container max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class="search__container__input">
+                    <label for="starting-location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                    <input v-model="start" id="starting-location" type="text" placeholder="Lieu de départ..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                </div>
+                <div class="search__container__input">
+                    <label for="ending-location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                    <input v-model="end" id="ending-location" type="text" placeholder="Lieu d'arrivée..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                </div>
+                <div class="search__container__input">
+                    <label for="starting-date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                    <input v-model="date" id="starting-date" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                </div>
+                <div class="search__container__input">
+                    <label for="number-of-passengers" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+                    <input v-model="passengers" title="Nombre de passagers" id="number-of-passengers" type="number" placeholder="Nombre de passagers..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                </div>
+                <div class="search__container__button">
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Recherche</button>
+                </div>
             </div>
-            <div class="search__container__input">
-                <label for="ending-location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                <input v-model="end" id="ending-location" type="text" placeholder="Lieu d'arrivée..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-            </div>
-            <div class="search__container__input">
-                <label for="starting-date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                <input v-model="date" id="starting-date" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-            </div>
-            <div class="search__container__input">
-                <label for="number-of-passengers"  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                <input v-model="passengers" title="Nombre de passagers" id="number-of-passengers" type="number" placeholder="Nombre de passagers..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-            </div>
-            <div class="search__container__button">
-                <button type="submit" @click="getTrips()" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Recherche</button>
-            </div>
-        </div>
+        </form>
     </div>
     <div class="result">
-        <ResultElement :result="trip" v-for="trip in trips.data" :key="trip"/>
+        <ResultElement :result="trip" v-for="trip in trips.data" :key="trip" />
     </div>
 </template>
 
@@ -39,13 +41,13 @@ import ResultElement from "../components/ResultElement.vue";
 
 export default {
     name: "SearchTrip",
-    data: ()=>({
+    data: () => ({
         trips: [],
         start: '',
         end: '',
         date: null,
         passengers: null,
-        results: []
+        arrival_datetime: null,
     }),
     components: {
         NavBar,
@@ -53,34 +55,23 @@ export default {
     },
     methods: {
         getTrips() {
-            fetch(`https://papotecar-backend.onrender.com/trips`, {
-                 method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization' : 'Bearer ' + 'Y2xmMTR5aDhmMDAwMDFnMW0xcjN2NjJnbQ.5oZsvsGFxqV2qUham98J0EavJzarDfaoXAJpJ1u94-jZ_LmSUkyT_HS8nxA9'
-                    }
+            let startingDate = null;
+            if (this.data !== null) {
+                const inputData = new Date(this.date);
+                startingDate = inputData.toISOString();
+            }
+            fetch(`https://papotecar-backend.onrender.com/trips?departure_location=${this.start}&arrival_location=${this.end}&departure_datetime=${startingDate}&max_passengers=${this.passengers}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + 'Y2xmYjY5bnM0MDAwMDFnNGcwbHhzZXlodg.T6VFmgUzf50EywAbWDjg0P6U98bLLPFIcdVxtcV3QQL9xGKx3eq-l4T1zUem'
+                }
             })
-            .then(response => response.json())
-            .then(data => {
-                this.trips = data;
-                this.results = data;
-                this.sendInformations();
-            })
-        },
-        sendInformations() {
-            if (this.date !== null) {
-                const date = new Date(this.date);
-                const startingDate = date.toISOString();             
-            }
-            if (this.passengers !== null) {
-                const passengers = this.passengers;
-            }
-            if (this.start !== '') {
-                const start = this.start;
-            }
-            if (this.end !== '') {
-                const end = this.end;
-            }
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.trips = data;
+                })
         }
     }
 };
@@ -100,14 +91,17 @@ export default {
     justify-content: flex-start;
     padding-top: 4rem;
 }
+
 .hero__container__title {
     font-weight: bold;
     margin: 0 auto;
     width: 75vw;
 }
+
 .search {
     margin-top: -2rem;
 }
+
 .search__container {
     display: flex;
     margin: 0 auto;
@@ -119,17 +113,21 @@ export default {
     padding: 0 1rem;
     width: 75vw;
 }
+
 .search__container label {
     display: none;
 }
+
 .search__container__input {
     margin: 0 .5rem;
 }
+
 .result {
     margin-top: 2rem;
     display: flex;
     flex-direction: column;
 }
+
 .result_container {
     display: flex;
     margin: 0 auto;
@@ -141,12 +139,13 @@ export default {
     padding: 0 1rem;
     width: 75vw;
 }
+
 @media (max-width: 1200px) {
     .search__container {
         flex-direction: column;
     }
+
     .search__container__input {
         margin: 1rem 0;
     }
-}
-</style>
+}</style>
