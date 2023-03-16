2083b5621cfb46c6d35dbf6313de65582dcb13b8
Auriane Labille <auriane.labille@gmail.com>
2023-03-16 14:50:51 +0100

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,11 +1,23 @@
 import { createWebHistory, createRouter } from 'vue-router';
 import SearchTrip from '../src/views/SearchTrip.vue';
+import Login from '../src/views/Login.vue';
+import Register from '../src/views/Register.vue';
 
 const routes = [
     {
       path: '/',
       name: 'Search Trip',
       component: SearchTrip,
+    },
+    {
+      path: '/connexion',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/inscription',
+      name: 'Register',
+      component: Register,
     },
     {
       path: '/:catchAll(.*)*',
