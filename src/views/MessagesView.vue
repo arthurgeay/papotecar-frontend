4690e6b616dde09e5bee2908ecd9ff9421fcdb43
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2023-04-13 10:57:14 +0200

    lint

--- src/views/MessagesView.vue
+++ src/views/MessagesView.vue
@@ -5,7 +5,8 @@
             <button type="button" class="return text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Retour au tableau de bord</button>
         </router-link>
         <div class="messages-container">
-            <div v-for="message in messages" id="toast-simple" :key="message" :class="initialId
+            <div
+v-for="message in messages" id="toast-simple" :key="message" :class="initialId
                 === message.user_id ? 'conductor' : 'other'" class="toast-simple flex w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
                 <div class="text-sm font-normal break-all">{{ message.content }}
                 </div>
