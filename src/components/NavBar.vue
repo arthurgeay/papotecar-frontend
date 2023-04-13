56fb902f322acda7424124cecba407e7d1d226b8
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2023-04-13 13:40:17 +0200

    format: nothing

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -49,8 +49,8 @@
             <li>
               <a
                 href="#"
-                @click.prevent="disconnect"
                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+                @click.prevent="disconnect"
                 >Déconnexion</a
               >
             </li>
