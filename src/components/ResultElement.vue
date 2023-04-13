a847774cd660ac8467fdefbb1dbfa18583b33c0c
Auriane Labille <auriane.labille@gmail.com>
2023-04-13 08:38:09 +0200

    Update ResultElement.vue

--- src/components/ResultElement.vue
+++ src/components/ResultElement.vue
@@ -37,20 +37,8 @@
                 </div>
             </div>
 
-            <!-- <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <div class="flex justify-end px-4 pt-4">
-                    <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
-                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>
-                    </button>
-                </div>
-                <div class="flex flex-col items-center pb-10">
-                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" :src="'https://i.pravatar.cc/150?u=' + result.driver.id" alt="profile picture"/>
-                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ result.driver.fullname }}</h5>
-                </div>
-            </div> -->
-
-            <div class="resultElement_information">
-                <div class="resultElement_information__container">
+            <div class="resultElement_information v">
+                <div class="resultElement_information__container bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <div class="resultElement_information__container__profilePicture">
                         <img :src="'https://i.pravatar.cc/150?u=' + result.driver.id" alt="profile picture">
                     </div>
@@ -58,7 +46,8 @@
                         <p>{{ result.driver.fullname }}</p>
                     </div>
                 </div>
-                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Réserver</button>
+                <button type="button" class="w-full resultElement_information__button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Se désinscrire</button>
+                <button type="button" class="w-full resultElement_information__button text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Messagerie</button>
             </div>
         </div>
     </div>
