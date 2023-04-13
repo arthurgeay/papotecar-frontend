c66afe6950c369e0d675739df644d807b2fa5086
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2023-04-13 15:02:57 +0200

    fix: margin on button

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,7 +2,7 @@
   <div class="dashboard">
     <NavBar />
     <router-link to="/trips/create">
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
+      <button type="button" class="text-white mt-12 ml-12 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
